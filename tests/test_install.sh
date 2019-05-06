f5f9bc293429dce09a5211c39a0d5af3014a11d4
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2019-05-06 19:42:25 +0200

    using uttemplate for testing

--- tests/test_install.sh
+++ tests/test_install.sh
@@ -8,7 +8,8 @@ virtualenv -p python3 "$ENV_DIR"
 
 #install needed packages:
 pip install cython
-pip install numpy #needed for testing
+pip install numpy # needed for testing
+pip install https://github.com/realead/uttemplate/zipball/master # needed for testing
 
 
 if [ "$1" = "from-github" ]; then
--- tests/unit_tests/test_1dsum.py
+++ tests/unit_tests/test_1dsum.py
@@ -6,48 +6,51 @@ import numpy as np
 import array
 import ctypes
 
+import uttemplate
 
+@uttemplate.from_templates([ps.pairwise_sum_1d, ps.kahan_sum_1d])
 class Sum1DTester(unittest.TestCase): 
 
-   def test_sum(self):
+   def template_sum(self, fun):
       a=np.array([1,2,3], dtype=np.float32)
-      s = ps.pairwise_sum_1d(a)
+      s = fun(a)
       self.assertAlmostEqual(s, 6.0)
 
-   def test_prec_sum(self):
+   def template_prec_sum(self, fun):
       a=np.ones(10**8, dtype=np.float32)
-      s = ps.pairwise_sum_1d(a)
+      s = fun(a)
       self.assertAlmostEqual(s, 1.0*10**8)
 
-   def test_bigger_stride(self):
+   def template_bigger_stride(self, fun):
      a=np.ones((5,5), dtype=np.float32)
      a[:,0]=3.0;
-     self.assertAlmostEqual(ps.pairwise_sum_1d(a[:,0]), 15.0)     
+     self.assertAlmostEqual(fun(a[:,0]), 15.0)     
      a[0,:]=2.0
-     self.assertAlmostEqual(ps.pairwise_sum_1d(a[0,:]), 10.0)
+     self.assertAlmostEqual(fun(a[0,:]), 10.0)
 
-   def test_sum_int(self):
+   def template_sum_int(self, fun):
       a=np.ones([1], dtype=np.int32)
       with self.assertRaises(BufferError) as context:
-            ps.pairwise_sum_1d(a)
+            fun(a)
       self.assertEqual("not float32 data", context.exception.args[0])
 
-   def test_sum_2d(self):
+   def template_sum_2d(self, fun):
       a=np.ones((2,2), dtype=np.float32)
       with self.assertRaises(BufferError) as context:
-            ps.pairwise_sum_1d(a)
+            fun(a)
       self.assertEqual("can handle only one-dimensional buffers", context.exception.args[0])
 
-   def test_array(self):
+   def template_array(self, fun):
       a=array.array('f', [4,3])
-      res=ps.pairwise_sum_1d(a)
+      res=fun(a)
       self.assertAlmostEqual(res, 7.0)
 
 
-   def test_ctypes(self):
+   def template_ctypes(self, fun):
       a = (ctypes.c_float * 2)()
       a[0]=1.0
       a[1]=2.0
-      res=ps.pairwise_sum_1d(a)
+      res=fun(a)
       self.assertAlmostEqual(res, 3.0)
+
 
